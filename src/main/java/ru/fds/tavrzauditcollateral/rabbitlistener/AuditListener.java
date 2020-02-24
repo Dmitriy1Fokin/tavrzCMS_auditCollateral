756cbd77243457ec17f97f9c16560bfb18343ce8
dmtriy <fokinds1@gmail.com>
2020-02-24 13:16:35 +0300

    added ability to execute audit

---
+++ src/main/java/ru/fds/tavrzauditcollateral/rabbitlistener/AuditListener.java
@@ -0,0 +1,25 @@
+package ru.fds.tavrzauditcollateral.rabbitlistener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import ru.fds.tavrzauditcollateral.service.AuditResultService;
+
+@Slf4j
+@EnableRabbit
+@Component
+public class AuditListener {
+
+    private final AuditResultService auditResultService;
+
+    public AuditListener(AuditResultService auditResultService) {
+        this.auditResultService = auditResultService;
+    }
+
+    @RabbitListener(queues = "${queue_name.execute_audit}")
+    public void executeAudit(){
+        log.debug("start executeAudit");
+        auditResultService.executeAuditInAllObjects();
+    }
+}
